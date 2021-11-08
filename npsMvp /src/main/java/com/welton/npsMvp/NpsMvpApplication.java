6666cbe033b5e219c7683546f62dcc9e3a61215e
xchrisw92 <christopher.welton@austincc.edu>
2021-11-08 10:49:03 -0600

    first commit

---
+++ npsMvp /src/main/java/com/welton/npsMvp/NpsMvpApplication.java
@@ -0,0 +1,13 @@
+package com.welton.npsMvp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NpsMvpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NpsMvpApplication.class, args);
+	}
+
+}
---
+++ npsMvp /src/test/java/com/welton/npsMvp/NpsMvpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.welton.npsMvp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NpsMvpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
